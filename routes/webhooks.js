5e251771e936182107d6b68652d721c45914ad90
Jason Leung <lalahehe@gmail.com>
2019-01-26 19:55:59 -0800

    separate follow callback fix

--- routes/webhooks.js
+++ routes/webhooks.js
@@ -15,44 +15,51 @@ var socketIO = {
   hub.lease_seconds=864000& \
   hub.challenge=HzSGH_h04Cgl6VbDJm7IyXSNSlrhaLvBi9eft3bw
 */
+function handleSubscriptionVerifyRequest(req, res, next) {
+
+    var hubmode = req.query['hub.mode'];
+    var hubtopic = req.query['hub.topic'];
+    var hubleaseseconds = req.query['hub.lease_seconds'];
+    var hubchallenge = req.query['hub.challenge'];
+
+    var tsr = new TwitchSubRequest({
+      mode: hubmode,
+      topic: hubtopic,
+      lease_seconds: hubleaseseconds,
+      challenge: hubchallenge,
+      full: JSON.stringify(req.query)
+    });
+    tsr.save(function(err) {
+      if (err) {
+        console.log(err);
+      } else {}
+    });
+
+    if (hubmode == 'subscribe') {
+      res.status(200).send(hubchallenge);
+    } else if (hubmode == 'unsubscribe') {
+      res.status(200).send('OK');
+    } else if (hubmode == 'denied') {
+      res.status(200).send('OK');
+    } else {
+      res.status(200).send('OK');
+    }
+}
+
 router.get('/callback', function(req, res, next) {
-
-  var hubmode = req.query['hub.mode'];
-  var hubtopic = req.query['hub.topic'];
-  var hubleaseseconds = req.query['hub.lease_seconds'];
-  var hubchallenge = req.query['hub.challenge'];
-
-
-  var tsr = new TwitchSubRequest({
-    mode: hubmode,
-    topic: hubtopic,
-    lease_seconds: hubleaseseconds,
-    challenge: hubchallenge,
-    full: JSON.stringify(req.query)
-  });
-  tsr.save(function(err) {
-    if (err) {
-      console.log(err);
-    } else {}
-  });
-
-  if (hubmode == 'subscribe') {
-    res.status(200).send(hubchallenge);
-  } else if (hubmode == 'unsubscribe') {
-    res.status(200).send('OK');
-  } else if (hubmode == 'denied') {
-    res.status(200).send('OK');
-  } else {
-    res.status(200).send('OK');
-  }
+  handleSubscriptionVerifyRequest(req, res, next);
 });
 
 router.post('/callback', function(req, res, next) {
 
   var body = req.body;
   console.log('callback body = ' + JSON.stringify(body));
-  socketIO.io.emit('callback', JSON.stringify(body));
+  socketIO.io.emit('topic', JSON.stringify(body));
 
+});
+
+router.get('/callback/follows', function(req, res, next) {
+  handleSubscriptionVerifyRequest(req, res, next);
 });
 
 router.post('/callback/follows', function(req, res, next) {
